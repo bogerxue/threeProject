2ee469fa23dfb7d33686be9ff684d060f13fd076
boger <bogerxue@foxmail.com>
2022-07-15 11:58:01 +0800

    "2022-7-15 11:57:38"

--- src/main/java/com/shangma/common/page/PageBean.java
+++ src/main/java/com/shangma/common/page/PageBean.java
@@ -1,8 +1,25 @@
 package com.shangma.common.page;
 
+import lombok.Data;
+
+import java.util.List;
+
 /**
  * @author BOGEY
- * 2022/7/15  11:05
+ * 2022/7/15  11:04
  */
-public class PageBean {
+@Data
+public class PageBean<T> {
+    private long total;
+
+    private List<T> data;
+
+    private PageBean(long total,List<T> data){
+        this.total = total;
+        this.data = data;
+    }
+
+    public static <T> PageBean<T> initData(long total,List<T> data){
+        return new PageBean<T>(total,data);
+    }
 }
